7f59a7283f2fe5ed373db9a987787163e519d8a8
jcreixell <jcreixell@gmail.com>
2015-10-23 18:40:57 +0200

    Completed tutorial

--- wiki.go
+++ wiki.go
@@ -4,12 +4,17 @@ import(
   "html/template"
   "io/ioutil"
   "net/http"
+  "regexp"
+  "errors"
 )
 
 type Page struct {
   Title string
   Body []byte
 }
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
   filename := p.Title + ".txt"
@@ -26,19 +31,40 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-  t, _ := template.ParseFiles(tmpl + ".html")
-  t.Execute(w, p)
+  err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil
+}
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) (http.HandlerFunc) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
+    }
+    fn(w, r, m[2])
+  }
 }
 
 func main() {
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  // http.HandleFunc("/save/", saveHandler)
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
   http.ListenAndServe(":8080", nil)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/" + title, http.StatusFound)
@@ -47,11 +73,22 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     p = &Page{Title: title}
   }
   renderTemplate(w, "edit", p)
 }
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  err := p.save()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
